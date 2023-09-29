1214af8f51cd7f6943eca8400bda15db309a8ba5
Yash Jejurkar <yashjejurkar2004@gmail.com>
2023-09-29 22:03:53 +0530

    After Designing Navigation Bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,34 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    
-
-    colors: {
-      'Demo': 'hsl(203,97.8%,35.1%)',
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
     },
-    extend: {},
+
+    extend: {
+
+      fontFamily: {
+        'be-vietnam-pro': ['Be Vietnam Pro', 'Sans'],
+      },
+
+      colors: {
+        // Primary Colors 
+        BrightRed: 'hsl(12, 88%, 59%)',
+        DarkBlue: ' hsl(228, 39%, 23%)',
+        // Neutrals 
+        DarkGrayishBlue: 'hsl(227, 12%, 61%)',
+        VeryPaleRed: 'hsl(13, 100%, 96%)',
+        VeryDarkBlue: 'hsl(233, 12%, 13%)',
+        VaryLightGray: 'hsl(0, 0%, 98%)',
+
+      },
+    },
+
   },
+
   plugins: [],
 }
 
