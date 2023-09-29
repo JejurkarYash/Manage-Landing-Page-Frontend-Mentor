6abffb7911135795dd789797a655a1623f1fb6c3
Yash Jejurkar <yashjejurkar2004@gmail.com>
2023-09-29 19:13:14 +0530

    Before Starting Development

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    
+
+    colors: {
+      'Demo': 'hsl(203,97.8%,35.1%)',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
