1f434d66b7e0723076d9a03ca19d8771b918ad71
Yash Jejurkar <yashjejurkar2004@gmail.com>
2023-10-15 21:55:29 +0530

    After Developing Feature Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,16 +12,19 @@ module.exports = {
     extend: {
 
       fontFamily: {
-        'be-vietnam-pro': ['Be Vietnam Pro', 'Sans'],
+        beVietnamPro: ['Be Vietnam Pro'],
       },
 
       colors: {
         // Primary Colors 
         BrightRed: 'hsl(12, 88%, 59%)',
+        BrightRedLight: 'hsl(12, 90%, 90%)',
+
+
         DarkBlue: ' hsl(228, 39%, 23%)',
         // Neutrals 
         DarkGrayishBlue: 'hsl(227, 12%, 61%)',
-        VeryPaleRed: 'hsl(13, 100%, 96%)',
+        VeryPaleRed: 'hsl(13, 88%, 80%)',
         VeryDarkBlue: 'hsl(233, 12%, 13%)',
         VaryLightGray: 'hsl(0, 0%, 98%)',
 
